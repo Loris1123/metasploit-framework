de97b01503b83f123cbc0d2201a31a9fa65c48cc
Loris1123 <molottermann@gmail.com>
2017-04-26 09:16:57 +0200

    Small fixes and improvements

--- modules/post/hardware/automotive/tp20_open_channel.rb
+++ modules/post/hardware/automotive/tp20_open_channel.rb
@@ -20,8 +20,9 @@ class MetasploitModule < Msf::Post
     ))
 
     register_options([
-      OptString.new('CANBUS', [true, "CAN Bus to perform scan on, defaults to connected bus", nil]),
-      OptString.new('SENDERID', [false, "CAN ID of the messages from Metasploit", "300"])
+      OptString.new('CANBUS', [true, "CAN Bus to open channel on, defaults to connected bus", nil]),
+      OptString.new('RECEIVERID', [false, "CAN ID of the messages received by Metasploit", "300"]),
+      OptString.new('DIAGPROTO', [false, "Diagnosticprotocol to use for the opened Channel. Default: 0x1 (KWP2000)", 0x1]),
     ], self.class)
 
     @device_id = nil
@@ -39,14 +40,14 @@ class MetasploitModule < Msf::Post
     close_channel    # Makes no sense here? When client is not alive anymore, close channel cannot be sent
   end
 
-  # Opens a TP 2.0 channel
-  # TODO: Improve to be more generic.
-  #       For now only the captured Gateway traffic is used.
+  # Opens a TP 2.0 channel by sending a C0 (Channel-Open) request with the wanted ID form SENDERID.
+  # The ID of the device will be parsed from the response
   def open_channel
-    request_id_high = datastore["SENDERID"][0].to_i(16)
-    request_id_low = datastore["SENDERID"][1..2].to_i(16)
+    request_id_high = datastore["RECEIVERID"][0].to_i(16)
+    request_id_low = datastore["RECEIVERID"][1..2].to_i(16)
 
-    response = client.automotive.cansend_and_wait_for_response(datastore["CANBUS"], "200", "21F", [0x1F, 0xC0, 0x00, 0x10, request_id_low, request_id_high, 0x01], {"MAXPKTS": 1})
+    # TODO: Check 200 and 21F
+    response = client.automotive.cansend_and_wait_for_response(datastore["CANBUS"], "200", "21F", [0x1F, 0xC0, 0x00, 0x10, request_id_low, request_id_high, datastore["DIAGPROTO"]], {"MAXPKTS": 1})
     if response["Packets"].size == 0
       print_error("Got no response from device. Could not open channel.")
       return false
@@ -59,8 +60,7 @@ class MetasploitModule < Msf::Post
   end
 
   # Keeps a TP 2.0 channel alive.
-  # TODO: Improve to be more generic.
-  #       For now only the captured Gateway traffic is used.
+  # Currently using A1 packages, which are settings. TODO: Check for a correct way of keeping alive.
   def keep_alive
     response = client.automotive.cansend_and_wait_for_response(datastore["CANBUS"], @device_id, datastore["SENDERID"], [0xA0, 0x0F, 0x8A, 0xFF, 0x32, 0xFF], {"MAXPKTS": 1})
     if response["Packets"][0]["DATA"] != ["A1", "0F", "8A", "FF", "4A", "FF"]
