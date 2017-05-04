a3a02ba2537c326a8366fd066a2acb6db67eef81
Loris1123 <molottermann@gmail.com>
2017-05-04 10:35:27 +0200

    TP 2.0 seems to work now. Probably there are some bugs

--- lib/msf/core/post/hardware/automotive/transport/tp20.rb
+++ lib/msf/core/post/hardware/automotive/transport/tp20.rb
@@ -56,6 +56,37 @@ class TP20
     @frame_counter = (@frame_counter + 1 ) % 16
   end
 
+  # Sends the given data to the bus and returns the response.
+  def send_and_wait_for_response(data)
+    send(data)
+    sleep 0.1   # Wait for the reception of packages.
+
+    # Get first frame of response
+    response = get_next_package
+
+
+    case response[0][0]
+    when "1"
+      # Single frame response
+      send_ack
+      return response[3..-1]
+    when "2"
+      # Multi frame response
+      # Calculate expected number of left packages.
+      length = response[1..2].join().to_i(16)
+      left_packages = ((length - 5) / 7.0).ceil
+      send_ack(left_packages+1)
+
+      result = response[3..7]
+      left_packages.times do
+        result += get_next_package[1..7]
+      end
+      return result
+    else
+      puts "Unknown Package: #{response}"
+    end
+  end
+
   # Opens a TP 2.0 channel by sending a C0 (Channel-Open) request with the wanted ID form RECEIVERID.
   # The ID of the device will be parsed from the response
   def open_channel
@@ -76,6 +107,14 @@ class TP20
     puts "Channel is open. Device want's to recieve packages at ID #{@device_id}"
     @channel_open = true
 
+    # Send Channel Parameters
+    @client.automotive.cansend(@canbus, @device_id, "A00F8AFF32FF")
+    sleep 0.1
+    response = @client.automotive.get_buffered_packages(@canbus, @tester_id, 1)
+    if response[0] != ["A1", "0F", "8A", "FF", "4A", "FF"]
+      puts("Got invalid response from device. Could not open channel.")
+    end
+
     keep_alive
     sleep 0.5 # To prevent timing issues. "send" could be faster than the first keep_alive
     return true
@@ -86,13 +125,16 @@ class TP20
   def keep_alive
     Thread.new do
       while @client.alive?
-        response = @client.automotive.cansend_and_wait_for_response(@canbus, @device_id, @tester_id, [0xA0, 0x0F, 0x8A, 0xFF, 0x32, 0xFF], {"MAXPKTS": 1})
-        if response["Packets"][0]["DATA"] != ["A1", "0F", "8A", "FF", "4A", "FF"]
+        @client.automotive.cansend(@canbus, @device_id, "A3")
+        sleep 0.1
+        response = @client.automotive.get_buffered_packages(@canbus, @tester_id, 1)
+        if response[0] != ["A1", "0F", "8A", "FF", "4A", "FF"]
           puts("Got invalid response from device. Could not open channel.")
         end
-        sleep 1.5
+        sleep 0.5
       end
     end
+    sleep 0.5   # Wait for the Keep-alive to be sent the first time
   end
 
   # Send an acknowledge.
@@ -103,7 +145,34 @@ class TP20
     @client.automotive.cansend(@canbus, @device_id, "B#{@ack_counter.to_s(16)}")
   end
 
+  # Returns the next package from the buffer.
+  # If it is a response of A3 (Keep Alive), the packet will be ignored
+  def get_next_package
 
+    # Check if packet is an ACK. -> Ignore for now
+    def ack?(packet)
+      if packet.size == 1 && packet[0][0] == 'B'
+        return true
+      end
+      return false
+    end
+
+    # Check if packet is a response of keep_alive -> Ignore
+    def keep_alive?(packet)
+      if packet.size == 6 && packet == ["A1", "0F", "8A", "FF", "4A", "FF"]
+        return true
+      end
+      return false
+    end
+
+    p = nil
+    loop do
+      p = @client.automotive.get_buffered_packages("slcan0", @tester_id, 1)[0]
+      break if !ack?(p) && !keep_alive?(p)
+    end
+
+    return p
+  end
 end
 
 end
