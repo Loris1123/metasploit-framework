3844024b2ac0dc2f72996d7452bbed3862b5b1a3
Loris1123 <molottermann@gmail.com>
2017-05-12 09:40:10 +0200

    Added skeletons for ISOTP and UDS

--- lib/msf/core/post/hardware/automotive/transport/isotp.rb
+++ lib/msf/core/post/hardware/automotive/transport/isotp.rb
@@ -5,8 +5,22 @@ module Hardware
 module Automotive
 module Transport
 
-  class ISOTP
+require 'msf/core/post/hardware/automotive/cantool'
 
+  class ISOTP
+    include Msf::Post::Hardware::Automotive::Cantool
+
+    def initialize(bus, options)
+      @bus = bus
+    end
+
+    def send(data)
+      raise "Not yet implemented"
+    end
+
+    def send_data_and_wait_for_response(data)
+      raise "Not yet implemented"
+    end
   end
 
 end
