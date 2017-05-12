2379cfea43e4a2465d7f85d5720426b9472d2975
Loris1123 <molottermann@gmail.com>
2017-05-12 09:40:10 +0200

    Added skeletons for ISOTP and UDS

--- lib/msf/core/post/hardware/automotive/application/uds.rb
+++ lib/msf/core/post/hardware/automotive/application/uds.rb
@@ -1,1 +1,27 @@
-# TODO
+# -*- coding: binary -*-
+module Msf
+class Post
+module Hardware
+module Automotive
+module Application
+
+class UDS
+
+  def initialize(client, bus, transport_protocol, options={})
+    @client = client
+    @bus = bus
+    # Setting transport protocol
+    case transport_protocol
+    when "TP20"
+      @client.automotive.set_transport_protocol(@bus, "TP20", options)
+    else
+      puts "UNKNOWN PROTOCOL"
+    end
+  end
+end
+
+end
+end
+end
+end
+end
